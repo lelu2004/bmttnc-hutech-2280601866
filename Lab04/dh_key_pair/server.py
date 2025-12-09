df38bbde28898e20b080faf9ed7b2434d0310dee
KimShin <llelu1808@gmail.com>
2025-12-09 13:50:39 +0700

    [add] lab04 dh_key_pair

---
+++ Lab04/dh_key_pair/server.py
@@ -0,0 +1,26 @@
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives import serialization
+
+def generate_dh_parameters():
+    parameters = dh.generate_parameters(generator=2, key_size=2048)
+    return parameters
+
+def generate_server_key_pair(parameters):
+    private_key = parameters.generate_private_key()
+    public_key = private_key.public_key()
+    return private_key, public_key
+
+def main():
+    parameters = generate_dh_parameters()
+    private_key, public_key = generate_server_key_pair(parameters)
+    
+    # Lưu Public Key của server ra file để client có thể đọc
+    with open("server_public_key.pem", "wb") as f:
+        f.write(public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        ))
+    print("Server public key generated and saved to 'server_public_key.pem'")
+
+if __name__ == "__main__":
+    main()
